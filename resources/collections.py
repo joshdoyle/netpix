7c06fa700b46cdefd7f689a2e202a75cb1424234
Josh Doyle <me@joshdoyle.us>
2020-02-15 20:20:25 -0600

    all collection routes without auth are working

--- resources/collections.py
+++ resources/collections.py
@@ -9,42 +9,90 @@ collections = Blueprint('collections', 'collections')
 # Index
 @collections.route('/', methods=['GET'])
 def collections_index():
-	collections = models.Collection.select()
-	collections_dict = [model_to_dict(c) for c in collections]
+	try:
+		collections = models.Collection.select()
+		collections_dict = [model_to_dict(c) for c in collections]
 
-	return jsonify(
-		data=collections_dict,
-		message=f"Retrieved {len(collections_dict)} collections.",
-		status=200
-	), 200
+		return jsonify(
+			data=collections_dict,
+			message=f"Retrieved {len(collections_dict)} collections.",
+			status=200
+		), 200
+	except Exception as e:
+		raise e
 
 # Show
 @collections.route('/<id>', methods=['GET'])	
 def get_collection(id):
-	collection = models.Collection.get_by_id(id)
-	collection_dict = model_to_dict(collection)
+	try:
+		collection = models.Collection.get_by_id(id)
+		collection_dict = model_to_dict(collection)
 
-	return jsonify(
-		data=collection_dict,
-		message=f"Found collection with id: {collection.id}.",
-		status=200
-	), 200
+		return jsonify(
+			data=collection_dict,
+			message=f"Found collection with id: {collection.id}.",
+			status=200
+		), 200
+	except Exception as e:
+		raise e
 
 
 # Create
 @collections.route('/', methods=['POST'])
 def create_collection():
-	payload = request.get_json()
-	print(payload)
+	try:
+		payload = request.get_json()
+		print(payload)
+		
+		collection = models.Collection.create(
+			name = payload['name'],
+			description = payload['description'],
+			category = payload['category']
+		)
+		collection_dict = model_to_dict(collection)
+		print('here is the collection:', collection_dict)
+		return jsonify(
+			data=collection_dict,
+			status={'message': 'Created collection'}
+		), 201
+
+	except Exception as e:
+		raise e
+
+# Destroy
+@collections.route('/<id>', methods=['Delete'])
+def delete_collection(id):
+	# TODO 2/15/20, 4:24 PM :Add logic to only let user delete their collections
+	try:
+		collection = models.Collection.get_by_id(id)
+		collection.delete_instance()
+
+		return jsonify(
+	        data={}, 
+	        message=f"Deleted collection with id: {id}",
+	        status=200
+      	), 200
 	
-	collection = models.Collection.create(
-		name = payload['name'],
-		description = payload['description'],
-		category = payload['category']
-	)
+	except Exception as e:
+		raise e
+
+# Update
+@collections.route('/<id>', methods=['PUT'])
+def update_collection(id):
+	payload = request.get_json()
+
+	collection = models.Collection.get_by_id(id)
+	collection.name = payload['name'] if 'name' in payload else None
+	collection.description = payload['description'] if 'description' in payload else None
+	collection.category = payload['category'] if 'category' in payload else None
+
+	collection.save()
+
 	collection_dict = model_to_dict(collection)
-	print('here is the collection:', collection_dict)
+
 	return jsonify(
 		data=collection_dict,
-		status={'message': 'Created collection'}
-	), 201
+		message=f'Updated collection with id: {collection.id}',
+		status=200
+	), 200	
+	
